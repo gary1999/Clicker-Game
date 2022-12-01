e9b22e3c08a6368438ca2dc63e19b4b41e2b3255
gary1999 <shengary1999@gmail.com>
2022-12-01 13:30:11 +1300

    Base code

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
-let total = 0;
+let total;
+
+if (localStorage.getItem('storageTotal')) {
+    total = parseInt(localStorage.getItem('storageTotal'));
+} else {
+    total = 0;
+}
 
 const increment = () => {
     total += 1;
+    localStorage.setItem('storageTotal', total);
     document.getElementById('total').innerHTML = total;
 };
 
 document.getElementById('click-increase').addEventListener('click', increment);
+document.getElementById('total').innerHTML = total;
